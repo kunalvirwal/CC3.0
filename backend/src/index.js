6f043f98b32235df27aa785915c0db2a20641a52
kunalvirwal <kunalvirwal@gmail.com>
2024-09-14 19:59:58 +0530

    Add middlewares folder and edit folder names
    Signed-off-by: kunalvirwal <kunalvirwal@gmail.com>

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,3 +2,19 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const path = require("path");
+
+const middlewares = require("./middlewares/Middlewares")
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("",require("./routes/Routes"))
+
+http.listen(PORT,(error)=>{
+    if (error) throw error;
+})
